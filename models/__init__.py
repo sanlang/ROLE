666ef43ce56bcdb421ce82a215a159cee60faf22
Victor Feng <sanlang@users.noreply.github.com>
2022-12-22 19:21:37 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .GraphEmb import *
-from .PoincareEmb import *
-from .GraphEmb_Direct import *
-from .ComplexEmb import *
-from .NestedEmb import *
-from .GraphEmb_Direct_Hetero import *
-from .GraphEmb_Direct_RotH import *
